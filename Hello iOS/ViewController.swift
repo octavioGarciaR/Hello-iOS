eb09b4bc185cb0df94e90347b488183c3505faaa
Octavio F Garcia Robles <octavio_garcia@foxmaster.local>
2016-09-05 20:26:50 -0500

    Initial commit

--- Hello iOS/ViewController.swift
+++ Hello iOS/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var messageLabel: UILabel!
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +23,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func changeMessage(sender: AnyObject) {
+        messageLabel.text = "Hola \(nameTextField.text!) 😁\n desde Swift!"
+        view.endEditing(true)
+    }
+    
+    //Comentary
 
 }
 
